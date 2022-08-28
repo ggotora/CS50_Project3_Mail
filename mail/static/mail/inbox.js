3b44c7220f2e76259ab02b2ee2964643db2a42c0
Gilbert Gotora <gilbertgotora2@gmail.com>
2022-08-28 22:27:12 +0200

    add postEmails function adnd load_mail conditionals

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -16,6 +16,8 @@ function compose_email() {
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
+  document.querySelector('#compose-form').addEventListener('submit', postEmails)
+
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
@@ -30,4 +32,42 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  if (mailbox === 'inbox'){
+    fetchEmails(`emails/${mailbox}`)
+  }
+
+  if (mailbox === 'sent'){
+    fetchEmails(`emails/${mailbox}`)
+  }
+
+  if (mailbox === 'archived'){
+    fetchEmails(`emails/${mailbox}`)
+  }
+  
+}
+
+function postEmails(e){
+  let recipients = document.querySelector('#compose-recipients').value;
+  let subject = document.querySelector('#compose-subject').value;
+  let body = document.querySelector('#compose-body').value;
+  e.preventDefault()
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+      load_mailbox('sent')
+  });
+}
+
+function fetchEmails(mail){
+  console.log('ad')
 }
